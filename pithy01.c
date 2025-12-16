855a0140381b11307d55b3289e5776fa8a08d9b3
Matt Teichman <teichman@uchicago.edu>
2025-12-16 14:00:57 -0600

    pithy01.c

--- pithy01.c
+++ pithy01.c
@@ -1,4 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main () { return 0; } 
+#define BSIZE 256
+
+int main ()
+{
+  const char filename[] = "pithy.txt";
+  FILE *fp;
+  char buffer[BSIZE];
+  char *r;
+
+  fp = fopen (filename, "r");
+
+  if (fp == NULL)
+    {
+      fprintf (stderr, "Unable to open file %s\n.", filename);
+      exit (1);
+    }
+
+  while (!feof (fp))
+    {
+      r = fgets (buffer, BSIZE, fp);
+      if (r == NULL)
+	{
+	  break;
+	}
+      printf ("%s", buffer);
+      printf ("dude\n");
+    }
+
+  fclose (fp);
+
+  return (0);
+} 
